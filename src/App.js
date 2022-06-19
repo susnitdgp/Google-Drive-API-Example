107eb6230a6ef8fed41dee3f475f6a2f01aac058
Susanta Goswami <susnitdgp@gmail.com>
2022-06-19 16:03:17 +0000

    Code QA Modified

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+  
         
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
-      </header>
-    </div>
+    
   );
 }
 
